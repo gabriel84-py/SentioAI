031d9587f355e1a2914db0d8c974573bcc69c4ea
gabriel84-py <hexahack.g8430@gmail.com>
2025-07-14 22:20:50 +0200

    data added

--- SentioAI/data_loader.py
+++ SentioAI/data_loader.py
@@ -2,6 +2,6 @@ import pandas as pd
 from datasets import load_dataset
 
 dataset = load_dataset("tblard/allocine")
-dataset["train"].to_pandas().to_csv("data/train.csv", index=False)
-dataset["validation"].to_pandas().to_csv("data/val.csv", index=False)
-dataset["test"].to_pandas().to_csv("data/test.csv", index=False)
+dataset["train"].to_pandas().to_csv("/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/data/train.csv", index=False)
+dataset["validation"].to_pandas().to_csv("/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/data/val.csv", index=False)
+dataset["test"].to_pandas().to_csv("/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/data/test.csv", index=False)

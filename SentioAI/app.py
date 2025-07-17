2c87e729be22d1d74056c7bbd425175f6dbf8ec4
gabriel84-py <hexahack.g8430@gmail.com>
2025-07-17 19:26:41 +0200

    ça marche !!!!!!

--- SentioAI/app.py
+++ SentioAI/app.py
@@ -1,9 +1,25 @@
 from SentioAI.data_loader import load_allocine_dataframe_from_csv
 from SentioAI.preprocessing import preprocess
 from SentioAI.vectorizer import vectorize, creer_vocab
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import pandas as pd
 import json
+import joblib
 
+model = joblib.load("model_sentioAI.pkl")
+vectorizer = joblib.load("vectorizer_sentioAI.pkl")
 
-with open("/Volumes/T7 Shield/dev_projects/data/vectorise.json", "r", encoding="utf-8") as f:
-    vectorized_data = json.load(f)
+# Exemple
+nouvelle_phrase = input("Donne ta phrase : ")
 
+# Prétraitement (le même que tu as appliqué à ton corpus)
+from SentioAI.preprocessing import preprocess
+
+tokens = preprocess(nouvelle_phrase)
+tfidf_dict = {mot: tokens.count(mot) / len(tokens) for mot in set(tokens)}
+tfidf_vect = vectorizer.transform([tfidf_dict])
+prediction = model.predict(tfidf_vect)
+
+print("Classe prédite :", prediction[0])
--- SentioAI/brouillon.py
+++ SentioAI/brouillon.py
@@ -38,3 +38,44 @@ data_to_save = {
 with open("/Volumes/T7 Shield/dev_projects/data/vectorise.json", "w", encoding="utf-8") as f:
     json.dump(data_to_save, f, ensure_ascii=False, indent=4)
 """
+
+
+
+
+"""
+
+with open("/Volumes/T7 Shield/dev_projects/data/vectorise.json", "r", encoding="utf-8") as f:
+    data_vectorized = json.load(f)
+
+X_train = data_vectorized["train"]
+X_val = data_vectorized["val"]
+X_test = data_vectorized["test"]
+
+# Chargement des labels
+train_df = pd.read_csv("/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/data/train.csv")
+val_df = pd.read_csv("/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/data/val.csv")
+test_df = pd.read_csv("/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/data/test.csv")
+
+y_train = train_df["label"].tolist()
+y_val = val_df["label"].tolist()
+y_test = test_df["label"].tolist()
+
+
+vectorizer = DictVectorizer(sparse=True)  # ou sparse=False si petit corpus
+
+X_train_vect = vectorizer.fit_transform(X_train)
+X_val_vect = vectorizer.transform(X_val)
+X_test_vect = vectorizer.transform(X_test)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train_vect, y_train)
+
+y_test_pred = model.predict(X_test_vect)
+test_accuracy = accuracy_score(y_test, y_test_pred)
+print("Test Accuracy:", test_accuracy)
+
+
+joblib.dump(model, "/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/SentioAI/model_sentioAI.pkl")
+joblib.dump(vectorizer, "/Users/gabrieljeanvermeille/PycharmProjects/SentioAI/SentioAI/vectorizer_sentioAI.pkl")
+
+"""
